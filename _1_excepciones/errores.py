8290cde5b788161090cddb76f5a781a065a9b409
Miguel Obando <mynameismiguel777@gmail.com>
2026-05-18 15:04:43 -0500

    feat(excepciones): agregar clases de errores personalizados para el sistema
    Se crean las excepciones:
    - UsuarioDuplicadoError
    - UsuarioNoEncontradoError
    - MateriaNoEncontradaError
    - DatoInvalidoError
    
    Estas permiten manejar fallos de forma controlada en el sistema

--- _1_excepciones/errores.py
+++ _1_excepciones/errores.py
@@ -1,8 +1,11 @@
 class UsuarioDuplicadoError(Exception):
     pass
+
 class UsuarioNoEncontradoError(Exception):
     pass
+
 class MateriaNoEncontradaError(Exception):
     pass
+
 class DatoInvalidoError(Exception):
     pass
